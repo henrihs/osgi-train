344f21823187c65acbb0f9ae0e6c6b5b9254e681
Henrik Heggelund Svendsen <hsvendsen@gmail.com>
2016-03-04 12:41:45 +0100

    Add initialization

--- no.ntnu.item.its.osgi.sensors.pn532/src/no/ntnu/item/its/osgi/sensors/pn532/PN532Factory.java
+++ no.ntnu.item.its.osgi.sensors.pn532/src/no/ntnu/item/its/osgi/sensors/pn532/PN532Factory.java
@@ -1,22 +1,42 @@
 package no.ntnu.item.its.osgi.sensors.pn532;
 
+import no.ntnu.item.its.osgi.sensors.common.exceptions.SensorInitializationException;
 import no.ntnu.item.its.osgi.sensors.pn532.impl.PN532;
 import no.ntnu.item.its.osgi.sensors.pn532.impl.PN532I2C;
 
 public class PN532Factory {
+	
+	private static int NUMBER_OF_TRIES = 10;
+	private static IPN532 instance;
 
-	public static IPN532 getInstance() {
-		PN532 pn = new PN532(new PN532I2C());
-		try {
-			// TODO Try this n times if it fails!
-			pn.begin();
-			pn.SAMConfig();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public static IPN532 getInstance() throws SensorInitializationException {
+		if (instance == null) {
+			instance = initializeInstance();
 		}
 		
-		return pn;
+		return instance;
+	}
+	
+	private static IPN532 initializeInstance() throws SensorInitializationException {
+		PN532 pn532 = new PN532(new PN532I2C());
+		while (!shouldGiveUp()) {
+			try {
+				pn532.begin();
+				if (pn532.SAMConfig())
+					return pn532;
+			} catch (InterruptedException e) {
+				try { Thread.sleep(200); } catch (InterruptedException e1) { e1.printStackTrace(); }
+				continue;
+			} finally {
+				
+			}
+		}
+		
+		throw new SensorInitializationException("Could not initialize PN532 sensor");
+	}
+	
+	private static boolean shouldGiveUp() {
+		return NUMBER_OF_TRIES-- < 0;
 	}
 	
 }
