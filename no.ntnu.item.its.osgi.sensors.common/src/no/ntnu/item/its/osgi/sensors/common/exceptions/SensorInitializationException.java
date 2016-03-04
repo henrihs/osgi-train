d3629f02a409da7af12241a9653a8ebfdd1ffb61
Henrik Heggelund Svendsen <hsvendsen@gmail.com>
2016-03-04 12:40:42 +0100

    Add exceptions to sensor.common bundle

---
+++ no.ntnu.item.its.osgi.sensors.common/src/no/ntnu/item/its/osgi/sensors/common/exceptions/SensorInitializationException.java
@@ -0,0 +1,17 @@
+package no.ntnu.item.its.osgi.sensors.common.exceptions;
+
+public class SensorInitializationException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public SensorInitializationException() {
+		super();
+	}
+	
+	public SensorInitializationException(String message) {
+		super(message);
+	}
+}
