f0490cad46b3f90d816c3eec754d48742228bb5b
Henrik Heggelund Svendsen <hsvendsen@gmail.com>
2016-04-15 14:17:33 +0200

    disable logging of acceleration data

--- no.ntnu.item.its.osgi.eventlogger/src/no/ntnu/item/its/osgi/eventlogger/EventLoggerActivator.java
+++ no.ntnu.item.its.osgi.eventlogger/src/no/ntnu/item/its/osgi/eventlogger/EventLoggerActivator.java
@@ -1,10 +1,9 @@
-package no.ntnu.item.its.osgi.test;
+package no.ntnu.item.its.osgi.eventlogger;
 
 import java.util.Hashtable;
 
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.Constants;
 import org.osgi.framework.ServiceReference;
 import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
@@ -16,17 +15,12 @@ import org.osgi.service.log.LogService;
 import org.osgi.util.tracker.ServiceTracker;
 import org.osgi.util.tracker.ServiceTrackerCustomizer;
 
-import no.ntnu.item.its.osgi.publishers.speed.VelocityPublisher;
-import no.ntnu.item.its.osgi.sensors.accel.AccelerationControlerMocker;
-import no.ntnu.item.its.osgi.sensors.color.ColorControllerMocker;
-import no.ntnu.item.its.osgi.sensors.common.enums.SensorNature;
 import no.ntnu.item.its.osgi.sensors.common.interfaces.AccelerationControllerService;
 import no.ntnu.item.its.osgi.sensors.common.interfaces.ColorControllerService;
 import no.ntnu.item.its.osgi.sensors.common.interfaces.MifareControllerService;
 import no.ntnu.item.its.osgi.sensors.common.interfaces.VelocityControllerService;
-import no.ntnu.item.its.osgi.sensors.mifare.MifareControllerMocker;
 
-public class TestActivator implements BundleActivator, EventHandler, LogListener {
+public class EventLoggerActivator implements BundleActivator, EventHandler, LogListener {
 
 	private static BundleContext context;
 
@@ -42,12 +36,12 @@ public class TestActivator implements BundleActivator, EventHandler, LogListener
 	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
 	 */
 	public void start(BundleContext bundleContext) throws Exception {
-		TestActivator.context = bundleContext;
+		EventLoggerActivator.context = bundleContext;
 		
 		topics = new String[] { 
 				ColorControllerService.EVENT_TOPIC, 
 				MifareControllerService.EVENT_TOPIC,
-				AccelerationControllerService.EVENT_TOPIC,
+//				AccelerationControllerService.EVENT_TOPIC,
 				VelocityControllerService.EVENT_TOPIC
 				};
 		Hashtable<String, Object> serviceProps = new Hashtable<String, Object>();
@@ -59,10 +53,6 @@ public class TestActivator implements BundleActivator, EventHandler, LogListener
 				LogReaderService.class,
 				new LogReaderTrackerCustomizer());
 		readerTracker.open();
-		
-		registerMifareMocker();
-		registerColorMocker();
-		registerAccelMocker();
 	}
 
 	/*
@@ -71,32 +61,10 @@ public class TestActivator implements BundleActivator, EventHandler, LogListener
 	 */
 	public void stop(BundleContext bundleContext) throws Exception {
 		readerTracker.close();
-		TestActivator.context = null;
+		EventLoggerActivator.context = null;
 	}
 	
-	private static void registerMifareMocker() {
-		MifareControllerService mockedController = new MifareControllerMocker(); 
-		Hashtable<String, Object> props = new Hashtable<String, Object>(); 
-		props.put(SensorNature.PROPERTY_KEY, SensorNature.SIMULATED);
-		props.put(Constants.SERVICE_RANKING, SensorNature.SIMULATED.ordinal());
-		context.registerService(MifareControllerService.class, mockedController, props);
-	}
 	
-	private static void registerColorMocker() {
-		ColorControllerService mockedController = new ColorControllerMocker(); 
-		Hashtable<String, Object> props = new Hashtable<String, Object>(); 
-		props.put(SensorNature.PROPERTY_KEY, SensorNature.SIMULATED);
-		props.put(Constants.SERVICE_RANKING, SensorNature.SIMULATED.ordinal());
-		context.registerService(ColorControllerService.class, mockedController, props);
-	}
-	
-	private static void registerAccelMocker() {
-		AccelerationControllerService mockedController = new AccelerationControlerMocker(); 
-		Hashtable<String, Object> props = new Hashtable<String, Object>(); 
-		props.put(SensorNature.PROPERTY_KEY, SensorNature.SIMULATED);
-		props.put(Constants.SERVICE_RANKING, SensorNature.SIMULATED.ordinal());
-		context.registerService(AccelerationControllerService.class, mockedController, props);
-	}
 
 	@Override
 	public void handleEvent(Event arg0) {
@@ -106,12 +74,12 @@ public class TestActivator implements BundleActivator, EventHandler, LogListener
 		else if (arg0.getTopic().equals(MifareControllerService.EVENT_TOPIC)) {
 			System.out.println(MifareControllerService.LOC_ID_KEY + ": " + arg0.getProperty(MifareControllerService.LOC_ID_KEY));
 		}
-		else if (arg0.getTopic().equals(AccelerationControllerService.EVENT_TOPIC)) {
-			System.out.println(
-					AccelerationControllerService.X_DATA_KEY + ": " + arg0.getProperty(AccelerationControllerService.X_DATA_KEY) + " " +
-					AccelerationControllerService.Y_DATA_KEY + ": " + arg0.getProperty(AccelerationControllerService.Y_DATA_KEY) + " " + 
-					AccelerationControllerService.Z_DATA_KEY + ": " + arg0.getProperty(AccelerationControllerService.Z_DATA_KEY));
-		}
+//		else if (arg0.getTopic().equals(AccelerationControllerService.EVENT_TOPIC)) {
+//			System.out.println(
+//					AccelerationControllerService.X_DATA_KEY + ": " + arg0.getProperty(AccelerationControllerService.X_DATA_KEY) + " " +
+//					AccelerationControllerService.Y_DATA_KEY + ": " + arg0.getProperty(AccelerationControllerService.Y_DATA_KEY) + " " + 
+//					AccelerationControllerService.Z_DATA_KEY + ": " + arg0.getProperty(AccelerationControllerService.Z_DATA_KEY));
+//		}
 		else if (arg0.getTopic().equals(VelocityControllerService.EVENT_TOPIC)) {
 			System.out.println(
 					VelocityControllerService.VX_KEY + ": " + arg0.getProperty(VelocityControllerService.VX_KEY));
