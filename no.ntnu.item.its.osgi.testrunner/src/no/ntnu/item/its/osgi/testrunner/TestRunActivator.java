a042e476928cac4e6160f6300b447c0289d41a3f
Henrik Heggelund Svendsen <hsvendsen@gmail.com>
2016-04-21 15:26:55 +0200

    use fully qual. names

---
+++ no.ntnu.item.its.osgi.testrunner/src/no/ntnu/item/its/osgi/testrunner/TestRunActivator.java
@@ -0,0 +1,54 @@
+package no.ntnu.item.its.osgi.testrunner;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+import no.ntnu.item.its.osgi.actuator.pwm.PwmActivator;
+import no.ntnu.item.its.osgi.eventlogger.EventLoggerActivator;
+import no.ntnu.item.its.osgi.publishers.accel.AccelPublisher;
+import no.ntnu.item.its.osgi.publishers.speed.VelocityPublisher;
+import no.ntnu.item.its.osgi.sensors.accel.AccelSensor;
+import no.ntnu.item.osgi.sensors.simulated.SimulationActivator;
+
+public class TestRunActivator implements BundleActivator {
+
+	private static BundleContext context;
+
+	static BundleContext getContext() {
+		return context;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#start(org.osgi.framework.BundleContext)
+	 */
+	public void start(BundleContext bundleContext) throws Exception {
+		TestRunActivator.context = bundleContext;
+		System.out.println("Starting testrun");
+		FrameworkUtil.getBundle(EventLoggerActivator.class).start();
+//		FrameworkUtil.getBundle(AccelSensor.class).start();
+		FrameworkUtil.getBundle(SimulationActivator.class).start();
+		FrameworkUtil.getBundle(VelocityPublisher.class).start();
+		FrameworkUtil.getBundle(AccelPublisher.class).start();
+//		Thread.sleep(3000);
+//		FrameworkUtil.getBundle(PwmActivator.class).start();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * @see org.osgi.framework.BundleActivator#stop(org.osgi.framework.BundleContext)
+	 */
+	public void stop(BundleContext bundleContext) throws Exception {
+//		FrameworkUtil.getBundle(PwmActivator.class).stop();
+//		Thread.sleep(3000);
+		FrameworkUtil.getBundle(EventLoggerActivator.class).stop();
+		FrameworkUtil.getBundle(AccelPublisher.class).stop();
+		FrameworkUtil.getBundle(VelocityPublisher.class).stop();
+//		FrameworkUtil.getBundle(AccelSensor.class).stop();
+		FrameworkUtil.getBundle(SimulationActivator.class).stop();
+		System.out.println("Testrun stopped");
+		TestRunActivator.context = null;
+	}
+
+}
