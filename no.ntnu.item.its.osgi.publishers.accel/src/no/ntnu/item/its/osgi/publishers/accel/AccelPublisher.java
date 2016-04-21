fb9294f8c8f22de69226cd889c52c22fdfdb31ce
Henrik Heggelund Svendsen <hsvendsen@gmail.com>
2016-04-21 15:29:04 +0200

    lowpass filtering of accelerometer data

--- no.ntnu.item.its.osgi.publishers.accel/src/no/ntnu/item/its/osgi/publishers/accel/AccelPublisher.java
+++ no.ntnu.item.its.osgi.publishers.accel/src/no/ntnu/item/its/osgi/publishers/accel/AccelPublisher.java
@@ -20,10 +20,12 @@ import no.ntnu.item.its.osgi.sensors.common.servicetrackers.SchedulerTrackerCust
 
 public class AccelPublisher implements PublisherService {
 	
-	public static final long SCHEDULE_PERIOD = 2;
+	public static final long SCHEDULE_PERIOD = 50;
 	private static final PublisherType TYPE = PublisherType.ACCEL;
+	private static final float ALPHA = 0.15f;
 
 	private Function<Void, Void> sensorReading;
+	private double[] previous;
 
 
 	public AccelPublisher() {		
@@ -74,7 +76,9 @@ public class AccelPublisher implements PublisherService {
 				try {
 					AccelerationControllerService acs = (AccelerationControllerService) 
 							AccelPubActivator.accelControllerTracker.getService();
-					int[] accelData = acs.getCalibratedData();
+					int[] accelDataBits = acs.getCalibratedData();
+					double[] accelData = convertToStandardUnits(accelDataBits);
+					accelData = lowPass(accelData, previous);
 					publish(accelData);
 					
 				} catch (Exception e) {
@@ -93,10 +97,11 @@ public class AccelPublisher implements PublisherService {
 		};
 	}
 	
-	private void publish(int[] accelData) {
+	private void publish(double[] accelData) {
 		if (!AccelPubActivator.eventAdminTracker.isEmpty()) {
 			Event accelEvent = createEvent(accelData);			
-			((EventAdmin) AccelPubActivator.eventAdminTracker.getService()).sendEvent(accelEvent);
+			((EventAdmin) AccelPubActivator.eventAdminTracker.getService()).postEvent(accelEvent);
+			previous = accelData;
 		}
 		
 		else if (!AccelPubActivator.logServiceTracker.isEmpty()) {
@@ -106,24 +111,41 @@ public class AccelPublisher implements PublisherService {
 		}
 	}
 
-	private Event createEvent(int[] accelData) {
+	private Event createEvent(double[] accelData) {
 		Map<String, Object> properties = new Hashtable<>();
 		properties.put(
 				AccelerationControllerService.TIMESTAMP_KEY, 
 				System.nanoTime());
 		properties.put(
 				AccelerationControllerService.X_DATA_KEY, 
-				accelData[0]/1024.0*AccelerationControllerService.GRAVITATIONAL_RATIO);
+				accelData[0]);
 		properties.put(
 				AccelerationControllerService.Y_DATA_KEY, 
-				accelData[1]/1024.0*AccelerationControllerService.GRAVITATIONAL_RATIO);
+				accelData[1]);
 		properties.put(
 				AccelerationControllerService.Z_DATA_KEY, 
-				accelData[2]/1024.0*AccelerationControllerService.GRAVITATIONAL_RATIO);
+				accelData[2]);
 		Event accelEvent = new Event(AccelerationControllerService.EVENT_TOPIC, properties);
 		return accelEvent;
 	}
 	
+	private double[] convertToStandardUnits(int[] accelDataFromBits) {
+		double[] accelData = new double[accelDataFromBits.length];
+		for (int i = 0; i < accelDataFromBits.length; i++) {
+			accelData[i] = accelDataFromBits[i]/1024.0*AccelerationControllerService.GRAVITATIONAL_RATIO;
+			
+		}
+		
+		return accelData;
+	}
 	
+	private double[] lowPass( double[] input, double[] output ) {
+	    if ( output == null ) return input;
+	     
+	    for ( int i=0; i<input.length; i++ ) {
+	        output[i] = output[i] + ALPHA * (input[i] - output[i]);
+	    }
+	    return output;
+	}
 
 }
